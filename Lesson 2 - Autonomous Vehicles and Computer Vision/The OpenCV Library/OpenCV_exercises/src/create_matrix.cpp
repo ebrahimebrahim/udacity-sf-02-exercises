d409edf118f761099789dfa8dee34055220e6f86
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2021-03-12 18:46:04 -0500

    complete lesson 2

--- Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
+++ Lesson 2 - Autonomous Vehicles and Computer Vision/The OpenCV Library/OpenCV_exercises/src/create_matrix.cpp
@@ -16,6 +16,9 @@ void createMatrix1()
     // STUDENT TASK :
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
     // depth of 8bit per channel. Then, set the first channel to 255 and display the result.
+    cv::Mat m3_8u;
+    m3_8u.create(nrows, ncols, CV_8UC3);
+    m3_8u.setTo(cv::Scalar(255,0,0));
 
 
     // show result
@@ -26,6 +29,10 @@ void createMatrix1()
 
     // STUDENT TASK :
     // Display the results from the STUDENT TASK above
+    std::string peupName = "peup";
+    cv::namedWindow(peupName, 1);
+    cv::imshow(peupName, m3_8u);
+    cv::waitKey(0);
 
 }
 
